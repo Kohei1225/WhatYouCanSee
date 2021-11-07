6879531edc896615d5cadbdc44b487be64e296c9
kaiboy333 <hirohiro20001217@gmail.com>
2021-11-07 21:16:33 +0900

    ワールド選択と整理 プレハブの衝突に注意！

--- Assets/Scripts/Object/CameraController.cs
+++ Assets/Scripts/Object/CameraController.cs
@@ -8,21 +8,27 @@ public class CameraController : MonoBehaviour
     //ズームが終わるまでの所要時間
     public float zoomSec = 1.0f;
 
-    //移動の速さ
-    private float moveSpeed;
     //最初の位置
     private Vector3 firstPos;
     //最後の位置
     private Vector3 lastPos; 
     //最終的な位置のゲームオブジェクト
     public GameObject lastPosObj;
+    //1秒に動くベクトル量
+    private Vector3 movePosPerSec;
 
-    //ズームの速さ
-    private float zoomSpeed;
     //最初のサイズ
     private float firstOrthographicSize;
     //最終的なサイズ
     public float lastOrthographicSize = 10;
+    //1秒に変化するサイズ量
+    private float changeSizePerSec;
+
+    private bool moveFin = false;
+    private bool sizeFin = false;
+
+    private float time = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,58 +36,38 @@ public class CameraController : MonoBehaviour
         firstPos = transform.position;
         lastPos = lastPosObj.transform.position;
         firstOrthographicSize = cam.orthographicSize;
-        int count = (int)(zoomSec / Time.deltaTime);
-        float distance = GetLength(firstPos, lastPos);
-        moveSpeed = distance / count;
-        float deltaSize = Mathf.Abs(lastOrthographicSize - firstOrthographicSize);
-        zoomSpeed = deltaSize / count;
+
+        movePosPerSec = (lastPos - firstPos) / zoomSec;
+        changeSizePerSec = (lastOrthographicSize - firstOrthographicSize) / zoomSec;
     }
 
     // Update is called once per frame
     void Update()
     {
+        time += Time.deltaTime;
         CameraZoom();
         CameraMove();
+        FinCheck();
     }
 
     private void CameraMove()
     {
-        Vector3 from = transform.position;
-        Vector3 to = lastPos;
-        if(GetLength(from, to) <= moveSpeed)
-        {
-            transform.position = to;
-        }
-        else
-        {
-            transform.Translate(GetVector(from, to) * moveSpeed);
-        }
+        transform.position = firstPos + movePosPerSec * time;
     }
 
     private void CameraZoom()
     {
-        if(cam.orthographicSize > lastOrthographicSize)
-        {
-            cam.orthographicSize -= zoomSpeed;
-        }
-        else
+        cam.orthographicSize = firstOrthographicSize + changeSizePerSec * time;
+    }
+
+    private void FinCheck()
+    {
+        if(time >= zoomSec)
         {
             GoalController goalCtrl = GameObject.Find("Goal").GetComponentInChildren<GoalController>();
             //goalCtrl.Set_canSuck(true);
             CameraController camCtrl = GetComponent<CameraController>();
             camCtrl.enabled = false;
         }
-    }
-
-    //fromからtoまでの単位ベクトルを返す
-    private Vector3 GetVector(Vector3 from, Vector3 to)
-    {
-        return (to - from).normalized;
-    }
-
-    //fromからtoまでの長さを返す
-    private float GetLength(Vector3 from, Vector3 to)
-    {
-        return (to - from).magnitude;
     }
 }

a09be53618532c48169eebc542a9eccb71d686da
関口晃平 <mccc.kohei@icloud.com>
2021-11-28 22:52:22 +0900

    アルファ値も変わるように変更

--- Assets/Scripts/Object/Ver3/ChangeChildrenColor.cs
+++ Assets/Scripts/Object/Ver3/ChangeChildrenColor.cs
@@ -7,21 +7,24 @@ public class ChangeChildrenColor : MonoBehaviour
 {
     /// <summary> アタッチされてるColorObjectスクリプト </summary>
     private ColorObjectVer3 _ColorObject;
+    /// <summary> アタッチされてるSpriteRenderer </summary>
+    private SpriteRenderer _SpriteRenderer;
 
-    /// <summary> 子オブジェクトのSpriteRendererの配列 </summary>
+    /// <summary> 子オブジェクトのの配列 </summary>
     [SerializeField] private SpriteRenderer[] _ChildrenSpriteRenderers;
 
     // Start is called before the first frame update
     void Start()
     {
         _ColorObject = GetComponent<ColorObjectVer3>();
+        _SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         ColorObjectVer3.OBJECT_COLOR3 colorType = _ColorObject.colorType;
-        Color color = _ColorObject.ChangeColorByType(colorType);
+        Color color = _SpriteRenderer.color;
 
         for(int i = 0; i < _ChildrenSpriteRenderers.Length; i++)
         {

0294a998d80f9e43b3db3b0ccba39c6438342e96
関口晃平 <mccc.kohei@icloud.com>
2021-12-03 16:10:26 +0900

    横から飛んでくる攻撃を作成

--- Assets/Scripts/Boss/Clow/ClowScript.cs
+++ Assets/Scripts/Boss/Clow/ClowScript.cs
@@ -14,7 +14,7 @@ public class ClowScript : BossBase
         FallAttack,
         AfterFall,
         Charge,
-        WindAttack,
+        WingAttack,
         LowFlyAttack,
         ReturnPostion,
         Damage,
@@ -61,6 +61,8 @@ public class ClowScript : BossBase
 
     /// <summary> 中心からクチバシまでの長さ(比率的な長さ) </summary>
     private const float _BeakPoint = 5.4f;
+
+    private float _WingAttackTime = 5.0f;
     #endregion
 
     #region property
@@ -79,7 +81,7 @@ public class ClowScript : BossBase
         _TaskList.DefineTask(TaskEnum.FallAttack, TaskFallAttackEnter, TaskFallAttackUpdate, TaskFallAttackExit);
         _TaskList.DefineTask(TaskEnum.AfterFall, TaskAfterFallEnter, TaskAfterFallUpdate, TaskAfterFallExit);
         _TaskList.DefineTask(TaskEnum.Charge, TaskChargeEnter, TaskChargeUpdate, TaskChargeExit);
-        _TaskList.DefineTask(TaskEnum.WindAttack, TaskWindAttackEnter, TaskWindAttackUpdate, TaskWindAttackExit);
+        _TaskList.DefineTask(TaskEnum.WingAttack, TaskWingAttackEnter, TaskWingAttackUpdate, TaskWingAttackExit);
         _TaskList.DefineTask(TaskEnum.LowFlyAttack, TaskLowFlyAttackEnter, TaskLowFlyAttackUpdate, TaskLowFlyAttackExit);
         _TaskList.DefineTask(TaskEnum.ReturnPostion, TaskReturnPosEnter, TaskReturnPosUpdate, TaskReturnPosExit);
         _TaskList.DefineTask(TaskEnum.Damage, TaskDamageEnter, TaskDamageUpdate, TaskDamageExit);
@@ -87,6 +89,7 @@ public class ClowScript : BossBase
 
         _WaitTime = 2.0f;
         _AttackInterval = 5.0f;
+        _ChargeTime = 3.0f;
 
         _State = StateEnum.Move;
         _CurrentHP = _MaxHp;
@@ -136,14 +139,14 @@ public class ClowScript : BossBase
     {
         Debug.Log("SelectTask");
         _AttackIntervalTimer.UpdateTimer();
-        Idle();
+        //Idle();
         if(!CanAttack)
         {
-            Debug.Log("Cant Attack!");
+            Debug.Log("Can't Attack!");
             return;
         }
-        Attack1();
-        _TaskList.AddTask(TaskEnum.Wait);
+        Attack2();
+        //_TaskList.AddTask(TaskEnum.Wait);
         //Attack2();
         //_TaskList.AddTask(TaskEnum.Wait);
         //Attack3();
@@ -164,13 +167,15 @@ public class ClowScript : BossBase
     {
         _TaskList.AddTask(TaskEnum.ReturnPostion);
         _TaskList.AddTask(TaskEnum.LowFlyAttack);
+        _TaskList.AddTask(TaskEnum.Wait);
     }
 
     /// <summary> 羽攻撃 </summary>
     public override void Attack3()
     {
+        _TaskList.AddTask(TaskEnum.ReturnPostion);
         _TaskList.AddTask(TaskEnum.Charge);
-        _TaskList.AddTask(TaskEnum.WindAttack);
+        _TaskList.AddTask(TaskEnum.WingAttack);
         _TaskList.AddTask(TaskEnum.Wait);
     }
 
@@ -326,38 +331,61 @@ public class ClowScript : BossBase
     #endregion
 
     #region Charge
+
+    int _FrameNumber = 0;
+    float _Xpos = 0f;
+
     void TaskChargeEnter()
     {
-        //Debug.Log("Charge");
+        Debug.Log("start charge");
         _WaitTimer.ResetTimer(_ChargeTime);
+
+        _FrameNumber = 0;
+        _Xpos = transform.position.x;
+        _AnimController.Play("Clow_Charge01",0,0);
     }
 
     bool TaskChargeUpdate()
     {
+        var pos = transform.position;
+        var dir = 1;
+
+        //フレームの数に応じて振動する
+        if ((_FrameNumber / 4) % 2 == 0)
+        {
+            dir = -1;
+        }
+        pos.x = _Xpos + dir * 0.25f;
+        transform.position = pos;
+
+        _FrameNumber++;
         _WaitTimer.UpdateTimer();
         return _WaitTimer.IsTimeUp;
     }
 
     void TaskChargeExit()
     {
-
+        Debug.Log("Finish Charge");
     }
     #endregion
 
-    #region WindAttack
-    void TaskWindAttackEnter()
+    #region WingAttack
+    void TaskWingAttackEnter()
     {
-
+        _AnimController.SetBool("IsWing", true);
+        _WaitTimer.ResetTimer(_WingAttackTime);
     }
 
-    bool TaskWindAttackUpdate()
+    bool TaskWingAttackUpdate()
     {
-        return true;
+        _WaitTimer.UpdateTimer();
+        return _WaitTimer.IsTimeUp;
     }
 
-    void TaskWindAttackExit()
+    void TaskWingAttackExit()
     {
-        //Debug.Log("羽攻撃完了!!");
+        _AnimController.SetBool("IsWing",false);
+        Debug.Log("Finish Wing");
     }
     #endregion
 
@@ -365,7 +393,9 @@ public class ClowScript : BossBase
     GameObject _TargetObject = null;
     void TaskLowFlyAttackEnter()
     {
-        //_LowAttackDir = _Player.transform.position - transform.position;
+        _AnimController.Play("Clow_LowFlying01", 0, 0);
+        _AnimController.SetBool("IsLowFly", true);
+        //右にいたら左へ、左にいたら右へ移動する
         if (CalcDistance(_RightSetPos,gameObject) < 1)
         {
             _TargetObject = _LeftSetPos;
@@ -381,15 +411,22 @@ public class ClowScript : BossBase
     {
         var pos = transform.position;
         pos.x += _Dir;
-        pos.y = _LowRoute.Evaluate(pos.x) * 30;
+
+        //var tmpY = Mathf.Lerp(0,1, _RightSetPos.transform.position.x - pos.x);
+        var tmpX = (transform.position.x - _LeftSetPos.transform.position.x)/(_RightSetPos.transform.position.x - _LeftSetPos.transform.position.x);
+        Debug.Log("tmp:" + tmpX);
+        pos.y = _LowRoute.Evaluate(tmpX);
+        pos.y *= _TargetObject.transform.position.y;
+        
         transform.position = pos;
-        //return _TargetObject.transform.position.y <= transform.position.y;
-        return Mathf.Abs(transform.position.x - _TargetObject.transform.position.x) < 0.1f;
+        return Mathf.Abs(_TargetObject.transform.position.x - transform.position.x) <= 1f;
     }
 
     void TaskLowFlyAttackExit()
     {
-       // Debug.Log("低空飛行攻撃完了!!");
+        _AnimController.SetBool("IsLowFly", false);
+        TurnTo(_Player);
+       // Debug.Log("Finish LowFly");
     }
     #endregion
 
@@ -397,6 +434,7 @@ public class ClowScript : BossBase
     public Vector3 _Vec;
     void TaskReturnPosEnter()
     {
+        
         //ステージの右側にいるか左側にいるかで対象を変える
         if(transform.position.x < 0)
         {
@@ -404,6 +442,7 @@ public class ClowScript : BossBase
         }
         else _TargetObject = _RightSetPos;
 
+        Debug.Log("I will return " + _TargetObject.name);
         //Debug.Log("I will return position");
         _Beak.SetActive(false);
 
@@ -417,11 +456,14 @@ public class ClowScript : BossBase
     {
         TurnTo(_Player);
 
+        if(_TargetObject.transform.position.y <= gameObject.transform.position.y)
+        {
+            return true;
+        }
+
         //対象まで一定のスピードで移動
         transform.position += _Vec * _MoveSpeed;
-
-        return _TargetObject.transform.position.y <= gameObject.transform.position.y;
-        //return CalcDistance(_TargetObject,gameObject) < 0.5f;
+        return false;
     }
 
     void TaskReturnPosExit()
@@ -431,6 +473,8 @@ public class ClowScript : BossBase
         //Debug.Log("移動終わり");
         _Head.SetActive(true);
         _Beak.SetActive(true);
+
+        Debug.Log("Finish Return");
     }
     #endregion
 
@@ -469,7 +513,7 @@ public class ClowScript : BossBase
 
     void TaskWaitExit()
     {
-        Debug.Log("HasWait");
+        //Debug.Log("Finish wait");
     }
     #endregion
 
